781af0746d00a72b943266369f4cdd4620f40f8b
Antonio Langiu <antonio.langiu@studenti.polito.it>
2017-05-19 23:45:48 +0200

    check format

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "github.com/AntonioLangiu/hknbot/common"
-    "github.com/AntonioLangiu/hknbot/bot"
+	"github.com/AntonioLangiu/hknbot/bot"
+	"github.com/AntonioLangiu/hknbot/common"
 )
 
 func main() {
-    configuration := common.LoadConfiguration()
-    go common.WebServer()
-    bot.LoadBot(configuration)
+	configuration := common.LoadConfiguration()
+	go common.WebServer()
+	bot.LoadBot(configuration)
 }
